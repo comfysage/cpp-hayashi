f7f40400d7b8b47654e04e59119da82d196a25b5
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-10-21 17:09:28 +0200

    Moved config serialization to ini format

--- src/util/config.h
+++ src/util/config.h
@@ -4,6 +4,7 @@
 #include "data/file.h"
 #include "util/constants.h"
 #include "serialization/serializer.h"
+#include "util/ini.h"
 
 struct Config {
   bool shallow_clone;
@@ -12,10 +13,20 @@ struct Config {
 template <>
 struct Serializer<Config> {
   void serialize(const Config &t, std::ostream &out) const {
-    Serializer<bool>().serialize(t.shallow_clone, out);
+    ini::Object object;
+    object.insert("config");
+    object.sections[object.currentsection]
+      .insert(ini::tuple_v{"shallow-clone", t.shallow_clone ? "true" : "false"});
+    Serializer<ini::Object>().serialize(object, out);
   }
   void deserialize(Config &t, std::istream &in) const {
-    Serializer<bool>().deserialize(t.shallow_clone, in);
+    ini::Object object;
+    Serializer<ini::Object>().deserialize(object, in);
+    std::string s_val = std::get<1>(
+        std::get<1>(
+          std::get<1>(
+            object.at("config") ).at("shallow-clone") ));
+    t.shallow_clone = s_val == "true";
   }
 };
 
