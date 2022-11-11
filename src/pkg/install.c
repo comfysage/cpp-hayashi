a65553c533b433ab25347173453820eb2ee83d46
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-11-11 11:05:24 +0100

    Better info on clone_pkg

--- src/pkg/install.c
+++ src/pkg/install.c
@@ -1,11 +1,21 @@
 #include "pkg/install.h"
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 result::Result clone_pkg(Pkg &pkg) {
 result::Await("cloning pkg.");
   if (path_exists(path::repo(pkg.name))) 
     result::Err(ERR_DIR_EXISTS, state.opts.force ? C_IGNORE : C_FAIL);
 
-  result::printf({ "url: ", pkg.url });
+  std::string s = "";
+  s = s + "cloning " + ANSI_COLOR_MAGENTA + pkg.name + ANSI_COLOR_RESET + " from " + ANSI_COLOR_YELLOW + pkg.url + ANSI_COLOR_RESET + " ...";
+  result::printf({ s });
 
   if (state.config.shallow_clone) {
     runin(REPO_ROOT, "git clone --depth 1 " + pkg.url + " -o " + pkg.name);
